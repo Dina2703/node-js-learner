00f5b485db2fc6b5579bcb6b00e0c28a03bcd00a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-14 16:20:04 -0500

    sending HTML page to the Browser

--- server/server.js
+++ server/server.js
@@ -1,24 +1,23 @@
 //first thing is to require http module
 const http = require("http");
-//createServer() method which creates a server. it's optional: we can store the server in a constant.
-//As an argument it takes ina callback function. this callback function is gonna run every time a request comes into our server. Inside this function we get access to two different objects: request and response objects.
-//The request object comes loaded full of information about the request such as the URL that is being requested, and request type and other stuff.
-//The response object we use to actually send a response to the user.
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
-  // console.log('request made');
   console.log(req.url, req.method);
 
-  // set header content type. The way we set a header is by using the response object and then using the mehtod setHeader(). There is a lots of different types of headers. We say text\plain, that means we're sending back some text to the browser.
-  //1 step setting the header for the content that being sent back to the browser
-  // res.setHeader("Content-Type", "text/plain");
+  //setting the header for the content
   res.setHeader("Content-Type", "text/html"); //html data
-  //2 step writing the content we want to send back to the browser
-  // res.write("hello. world"); plain text
-  res.write("<h4>hello. world</h4>");
-  res.write("<p>hello, again world</p>");
-
-  //3 step ending the response, which then sends to the browser
-  res.end();
+  //send an html file. data - means data from the file we're reading from
+  fs.readFile("../views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end(data);
+    } else {
+      // res.write(data); //when sending multiple things
+      // res.end();
+      res.end(data); //short way when sending only one things into response.
+    }
+  });
 });
 //to listen for requests we need to pass, first port number, then hostname, function which fires when we start listening
 server.listen(3000, "localhost", () => {
