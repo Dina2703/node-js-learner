491b8a7983bdf886fb86d96208f14636bee40f6d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-15 13:34:09 -0500

    Routing and sending HTML page

--- app.js
+++ app.js
@@ -9,5 +9,14 @@ app.listen(3000);
 //first arg is what path or URL you want to listen to. second arg is callback funtion which takes request and response object.
 //send() auto sets the content type header(don't need to do setHead()) and status code for us.
 app.get("/", (req, res) => {
-  res.send("<p>Hello from the express</p>"); 
+  //res.send("<p>Hello from the express</p>");
+  res.sendFile("./views/index.html", {
+    root: __dirname,
+  });
+}); //if the path is relative we need to tell Express where is it relative from, because default is going to look for an absolute path, the path from the root of our computer. So what we need to do is pass second arg which is an obj, where we can specify what the root should be(root: __dirname) which is NODE_JS folder.
+app.get("/about", (req, res) => {
+  //res.send("<p>About page from Express</p>");
+  res.sendFile("./views/about.html", {
+    root: __dirname,
+  });
 });
