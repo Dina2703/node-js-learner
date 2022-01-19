73f254c696dd1410388b87e83bed08b6382d4498
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-18 21:20:56 -0500

    outputting documents in views

--- app.js
+++ app.js
@@ -27,68 +27,22 @@ app.set("view engine", "ejs"); // automatically Express and ejs is going to look
 app.use(express.static("public")); // that means anything inside public folder is gonna be available as a static file to the browser.
 app.use(morgan("dev")); //third-party middleware
 
-//mongoose and mongo sandbox routes
-//adding a new blog. first create an instance of Blog, when use save() method on that instance(because, we need to save the new instance/blog to the collection)
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "new blog #2",
-    snippet: "about my new blog",
-    body: "Lorem ipsum dolor sit amet consectetur",
-  }); //use the Blog modal to create a new instance of it(since we're adding a new blog to the collection), pass an object with properties wh're defined in Scheme structure
-
-  blog
-    .save()
-    .then((result) => {
-      res.send(result); // we need to send back the result, so we can see it in the browser.
-    })
-    .catch((err) => {
-      console.log(err); // just in case if there is one.
-    });
-});
-
-// get all the blogs from the collection.
-// when we get the data, we call the find()method on the collection name, which is Blog (since we need all the data from that collection).
-app.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-//find the sinlge blog from the collection
-app.get("/single-blog", (req, res) => {
-  Blog.findById("61e76aad885009380a77d03b")
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
+//routes
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "title name one",
-      snippet: "Lorem ipsum dolor sit amet consectetur ",
-    },
-    {
-      title: "title name two",
-      snippet: "Lorem ipsum dolor sit amet consectetur ",
-    },
-    {
-      title: "title name three",
-      snippet: "Lorem ipsum dolor sit amet consectetur ",
-    },
-  ];
-  res.render("index", { title: "home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "about" });
+});
+
+//blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 }) //it's gonna sort from the newest to the oldest(-1)
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result }); //passing result as value of blogs(property name), since index view expects blogs
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
