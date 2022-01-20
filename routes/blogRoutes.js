6ca54446170259ae01b235fceb82afeac1ff4435
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-20 12:42:55 -0500

    Controllers

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,63 +1,20 @@
 const express = require("express");
 const router = express.Router(); //create a new instance of a router object.
-const Blog = require("../models/blog");
-
-
+const blogController = require("../controllers/blogController");
 
 //blog routes
 //GET request
-router.get("/", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 }) //it's gonna sort from the newest to the oldest(-1)
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result }); //passing result as value of blogs(property name), since index view expects blogs
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/", blogController.blog_index);
 
 //POST request
-router.post("/", (req, res) => {
-  const blog = Blog(req.body);
+router.post("/", blogController.blog_create_post);
 
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.get("/create", (req, res) => {
-  res.render("create", { title: "create a new Blog" });
-});
+router.get("/create", blogController.blog_create_get);
 
 //Route Parameters
-router.get("/:id", (req, res) => {
-  const id = req.params.id; //to excract the id use params. Whatever you call your variable comes after req.params.varName
-  console.log(id);
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/:id", blogController.blog_details);
 
 //DELETE request
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.delete("/:id", blogController.blog_delete);
 
 module.exports = router;
